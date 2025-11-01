d9bf531eaea0822fa41266ebf06a7c1ba3d493a5
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2025-11-01 18:45:53 -0300

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,6 @@
 """
-Pacote de testes para Gerador e Analisador de G-code CNC
+Pacote de testes para o Gerador e Analisador de G-code CNC
 """
+
+__version__ = "1.0.0"
+__author__ = "Misael Andrejezieski."
